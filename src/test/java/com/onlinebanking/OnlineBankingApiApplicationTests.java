13697e8ccc262a35673073f49240565f712c018a
Sourav Modak <72427070+official-srv-modak@users.noreply.github.com>
2025-02-25 15:03:41 -0500

    Initial commit

---
+++ src/test/java/com/onlinebanking/OnlineBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
